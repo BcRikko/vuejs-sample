6f7be73e89ca2b1f276fafa30f3aed6542050951
BcRikko <s.shin1106@gmail.com>
2016-10-03 13:25:05 +0900

    Header Component追加

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <span>Todo List</span>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: #41B883;
+  color: #fff;
+
+  height: 100px;
+}
+
+header span {
+  font-size: 35px;
+  padding-left: 35px;
+  line-height: 100px;
+}
+</style>
